285c555c1b19709849de6f22a8b4b7cc87c40511
石坚 <shijian@shijiandemacbook-pro.local>
2015-11-10 11:07:25 +0800

    Initial Commit

---
+++ GCD/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GCD
+//
+//  Created by 石坚 on 15/11/10.
+//  Copyright © 2015年 石坚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
